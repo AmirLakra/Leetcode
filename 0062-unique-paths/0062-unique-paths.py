55d1acdb01556dc74e2f5b07d25d6cacdd5e6cac
AmirLakra <amirlakra016@gmail.com>
2026-07-25 07:42:02 +0530

    Time: 3 ms (19.62%), Space: 19.9 MB (9.41%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Top Down approach (memoization)
+        memo = {(0,0) : 1}
+        def paths(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            elif i < 0 or j < 0 or i == m or j == n:
+                return 0
+            else:
+                val = paths(i-1,j) + paths(i,j-1)
+                memo[(i,j)] = val
+                return val
+        return paths(m-1,n-1)
